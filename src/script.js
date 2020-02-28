b231ca47b111cd15403b9cee678a8043698822be
Vettiger, Linus <linus.vettiger@novartis.com>
2020-02-28 19:39:47 +0100

    Restructured main function not to use global scope

--- src/script.js
+++ src/script.js
@@ -1,65 +1,74 @@
-import createList, { mount as mountListView, update as updateListView } from './components/listview';
+import createList from './components/listview';
 import { mount as mountDetailsView, update as updateDetailsView } from './components/detailsView';
+
 import store from "./redux/store";
 import { addTodo, deleteTodo, selectTodo, updateTodo } from "./redux/actions";
 import createInput from "./components/input";
 
+(() => {
+  const appRoot = document.createElement('div');
+  appRoot.id = 'app-root';
 
-const appRoot = document.createElement('div');
-appRoot.id = 'app-root';
-document.body.appendChild(appRoot); // Setup app-root-node
-
-renderInputField();
-renderListView();
-renderDetailView();
-
-function renderInputField() {
-  const input = createInput({
-    placeholder: 'Create your first todo',
-  });
-  input.mount(appRoot);
-  const changeHandler = event => {
-    if (event.target.value.length > 0) {
-      store.dispatch(addTodo(event.target.value));
-      input.update({value: ''}); // Clear input field
+  setupInput: {
+    try {
+      const input = createInput({
+        placeholder: 'Create your first todo',
+      });
+      input.mount(appRoot);
+      const changeHandler = event => {
+        if (event.target.value.length > 0) {
+          store.dispatch(addTodo(event.target.value));
+          input.update({value: ''}); // Clear input field
+        }
+      };
+      input.update({onChange: changeHandler}); // Attach change listener
+    } catch (e) {
+      break setupInput;
     }
-  };
-  input.update({ onChange: changeHandler }); // Attach change listener
-}
-
-function renderListView() {
-  const handleSelect = (selectedId) => {
-    store.dispatch(selectTodo(selectedId))
-  };
-  const handleDelete = (listItemId) => {
-    store.dispatch(deleteTodo(listItemId));
-  };
-  const list = createList({
-    items: store.getState().todos,
-    selectedItem: store.getState().selectedTodo,
-    onSelectionChange: handleSelect,
-    onDelete: handleDelete,
-  });
-  const unsubscribe = store.subscribe(store => {
-    list.update({ items: store.todos, selectedItem: store.selectedTodo })
-  });
-  document.addEventListener('close', unsubscribe);
-  list.mount(appRoot);
-}
-
-function renderDetailView() {
-  const handleChange = (changedItem) => {
-    store.dispatch(updateTodo(changedItem));
-  };
-
-  const unsubscribe = store.subscribe((newStore, oldStore) => {
-    // Check if rerender is necessary
-    if (newStore.selectedTodo !== oldStore.selectedTodo
-      || oldStore.todos.byId[newStore.selectedTodo] !== newStore.todos.byId[newStore.selectedTodo]) {
-      updateDetailsView({ selectedItem: newStore.todos.byId[newStore.selectedTodo] })
+  }
+  setupListView: {
+    try {
+      const handleSelect = (selectedId) => {
+        store.dispatch(selectTodo(selectedId))
+      };
+      const handleDelete = (listItemId) => {
+        store.dispatch(deleteTodo(listItemId));
+      };
+      const list = createList({
+        items: store.getState().todos,
+        selectedItem: store.getState().selectedTodo,
+        onSelectionChange: handleSelect,
+        onDelete: handleDelete,
+      });
+      const unsubscribe = store.subscribe(store => {
+        list.update({items: store.todos, selectedItem: store.selectedTodo})
+      });
+      document.addEventListener('close', unsubscribe);
+      list.mount(appRoot);
+    } catch (e) {
+      break setupListView;
     }
-  });
-  document.addEventListener('close', unsubscribe);
+  }
+  setupDetailsView: {
+    try {
+      const handleChange = (changedItem) => {
+        store.dispatch(updateTodo(changedItem));
+      };
 
-  mountDetailsView(appRoot)({ selectedItem: {}, onChange: handleChange });
-}
+      const unsubscribe = store.subscribe((newStore, oldStore) => {
+        // Check if rerender is necessary
+        if (newStore.selectedTodo !== oldStore.selectedTodo
+          || oldStore.todos.byId[newStore.selectedTodo] !== newStore.todos.byId[newStore.selectedTodo]) {
+          updateDetailsView({selectedItem: newStore.todos.byId[newStore.selectedTodo]})
+        }
+      });
+      document.addEventListener('close', unsubscribe);
+
+      mountDetailsView(appRoot)({selectedItem: {}, onChange: handleChange});
+    } catch (e) {
+      break setupDetailsView;
+    }
+  }
+
+  document.body.appendChild(appRoot); // Setup app-root-node
+})();
